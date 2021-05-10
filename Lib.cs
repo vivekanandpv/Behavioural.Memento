f8345f36569a736bf2f9df4e7806623bcad44f72
vivek <vivek@athenaeum.in>
2021-05-10 19:29:53 +0530

    Add project files.

---
+++ Lib.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Behavioural.Memento
+{
+    public class Document
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public DocumentMemento Save()
+        {
+            return new DocumentMemento(Title, Body);
+        }
+
+        public void Undo(DocumentMemento memento)
+        {
+            Title = memento.Title;
+            Body = memento.Body;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title} {Environment.NewLine}Body: {Body} {Environment.NewLine}-------------------------------------";
+        }
+    }
+
+    public class DocumentMemento
+    {
+        private readonly string _title;
+        private readonly string _body;
+
+        public DocumentMemento(string title, string body)
+        {
+            _title = title;
+            _body = body;
+        }
+
+        public string Title => _title;
+        public string Body => _body;
+    }
+
+    public class Curator
+    {
+        private readonly Stack<DocumentMemento> history = new Stack<DocumentMemento>();
+
+        public void Save(Document document)
+        {
+            history.Push(document.Save());
+        }
+
+        public void Undo(Document document)
+        {
+            if (history.Count > 0)
+            {
+                document.Undo(history.Pop());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Behavioural.Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curator curator = new Curator();
+            Document doc = new Document { Title = "Initial Title", Body = "Initial Body" };
+            
+
+            curator.Save(doc);
+
+            curator.Undo(doc); // No effect
+            Console.WriteLine(doc);
+
+            doc.Title = "New Title 1";
+            curator.Save(doc);
+
+            Console.WriteLine(doc); //  New Title 1
+
+            doc.Body = "New body 1";    
+            Console.WriteLine(doc);
+
+            curator.Undo(doc);  //  New Title 1
+
+            Console.WriteLine(doc);
+
+        }
+    }
+}
